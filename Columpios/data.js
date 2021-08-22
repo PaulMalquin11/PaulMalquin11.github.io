1a31c8932be69c6de3f90c52e6825c831736e0f5
PaulMalquin11 <87738066+PaulMalquin11@users.noreply.github.com>
2021-08-21 21:37:41 -0500

    Recorridos adicionales

---
+++ Columpios/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-columpios-carapaz-",
+      "name": "Columpios Carapaz ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -2.851779266070185,
+        "pitch": 0.16582498018035707,
+        "fov": 1.1731136983679884
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.3929377877036266,
+          "pitch": -0.2608599261795028,
+          "title": "Culumpios Carapaz",
+          "text": "Aquí se pueden realizar una variedad de actividades como columpio infinito, parrilladas entre otras, además de conectarse con la naturaleza"
+        }
+      ]
+    }
+  ],
+  "name": "Columpios Carapaz",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ Piscina/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-piscina",
+      "name": "Piscina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.053484853416790656,
+        "pitch": 0.5141639853537185,
+        "fov": 1.1731136983679884
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.9507123879233959,
+          "pitch": 0.06796904480282251,
+          "title": "Piscina",
+          "text": "El GAD Municipal San Pedro de Huaca impulsa un proyecto de restauración de esta piscina que es un lugar que se debe visitar dentro del cantón"
+        }
+      ]
+    }
+  ],
+  "name": "Piscina",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
